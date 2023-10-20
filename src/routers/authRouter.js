1b80b7ea704466b3dc39be6afad3b5a142c3adb8
Shahin Mia <shahin01777296933@gmail.com>
2023-10-20 21:46:50 +0600

    Delete src directory

--- src/routers/authRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { handleLogin, handleLogout } = require('../controllers/authController');
-
-const authRouter = express.Router();
-
-authRouter.post('/login', handleLogin);
-authRouter.post('/logout', handleLogout);
-
-module.exports = authRouter;
