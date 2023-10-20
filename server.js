f2f898d0ed9b9562f22d2f0977710591d88eb353
Shahin Mia <shahin01777296933@gmail.com>
2023-10-20 22:25:53 +0600

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ require('dotenv').config();
 const port = process.env.PORT;
 
 app.listen(port, async () => {
-  console.log(`server is running at http://localhost:${port}`);
+  console.log(`server is running`);
   await connectDB();
 });
